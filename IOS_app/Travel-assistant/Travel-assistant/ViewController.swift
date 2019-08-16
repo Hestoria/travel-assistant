f745d8e639c0c312f66cf8afa9ba4bd70be7534a
andy chan <ccyandy9582@gmail.com>
2019-08-16 11:26:07 +0800

    create android and ios file

---
+++ IOS_app/Travel-assistant/Travel-assistant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Travel-assistant
+//
+//  Created by andy chan on 16/8/2019.
+//  Copyright © 2019 FYP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IOS_app/Travel-assistant/Travel-assistantTests/Travel_assistantTests.swift
@@ -0,0 +1,34 @@
+//
+//  Travel_assistantTests.swift
+//  Travel-assistantTests
+//
+//  Created by andy chan on 16/8/2019.
+//  Copyright © 2019 FYP. All rights reserved.
+//
+
+import XCTest
+@testable import Travel_assistant
+
+class Travel_assistantTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
